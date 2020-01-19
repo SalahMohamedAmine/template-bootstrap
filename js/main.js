f917de59af8dc9a33016f792474837b1cb53d604
amine44482 <amine44482@gmail.com>
2020-01-19 17:15:16 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+   $(window).on('load', function()
+{ 	
+	$(".loadingg").fadeOut(5000);
+ })
+ 
+ //nice scroll
+ $(function() {  
+    $("body").niceScroll();
+});
+
+$(document).ready(function()
+{
+	$('.carousel').carousel({
+  		interval: 5000
+	});
+});
+
+
+});
